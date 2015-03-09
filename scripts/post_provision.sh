2d080802227371e74ca98771b69598c173ad1500
tigershen23 <tigershen23@gmail.com>
2015-03-08 18:48:40 -0700

    Add vundle to post-provision script

--- scripts/post_provision.sh
+++ scripts/post_provision.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Get my dotfiles
 git clone https://github.com/tigershen23/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
